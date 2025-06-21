c2a88f0789d08812eafce986485f09d8a26d3e72
financelabceo <ian@financelab.is>
2025-06-20 21:32:52 -0500

    message

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder, OrdinalEncoder
+from sklearn.ensemble import GradientBoostingClassifier
